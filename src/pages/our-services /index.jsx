74e551d274a6abd4e1240f17b294386faf3274d9
user <user@usernoMacBook-Pro.local>
2023-02-22 03:22:59 +0600

    feat: create our services page

---
+++ src/pages/our-services /index.jsx
@@ -0,0 +1,24 @@
+/* eslint-disable no-irregular-whitespace */
+import FeedbackForm from '../../components/form/FeedbackForm'
+
+const OurServices = () => {
+   return (
+      <div>
+         <div>
+            <h1>Наши услуги</h1>
+            <p>
+               Наши сотрудники прошли сертифицированные тренинги в Учебных
+               центрах ГК DoorHan в г. Москва, г. Алматы, г. Астаны
+               а так же успешно сдали экзамены и являются обладателями
+               сертификатов по направлениям «Воротные системы, ролл ставни, ролл
+               ворота, автоматические системы», «Монтаж автоматики».
+            </p>
+         </div>
+
+         <div>cards</div>
+         <FeedbackForm />
+      </div>
+   )
+}
+
+export default OurServices
