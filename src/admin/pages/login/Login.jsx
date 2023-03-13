65eb9dd6daef934d6b9f247a14021f04f93173b1
syimyk.jetibaev99@gmail.com <user@Syimyks-macbook.local>
2023-03-14 03:12:18 +0600

    feat: add login methods and also add logOut func

--- src/admin/pages/login/Login.jsx
+++ src/admin/pages/login/Login.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useEffect } from 'react'
 
 import { cilLockLocked, cilUser } from '@coreui/icons'
 import CIcon from '@coreui/icons-react'
@@ -15,11 +16,76 @@ import {
    CInputGroupText,
    CRow,
 } from '@coreui/react'
-import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form'
+import { useDispatch } from 'react-redux'
+import { Link, useNavigate } from 'react-router-dom'
 
+import { useLoginAdminMutation } from '../../../store/admin/auth/authApi'
+import { authActions } from '../../../store/admin/auth/authSlice'
+import { ROLES } from '../../../utils/constants'
 import nasa from '../../assets/images/nasa.jpeg'
 
 const Login = () => {
+   const navigate = useNavigate()
+   const dispatch = useDispatch()
+
+   const [loginAdmin, { data, isLoading, isError, isSuccess, error }] =
+      useLoginAdminMutation()
+
+   const {
+      register,
+      formState: { errors },
+      handleSubmit,
+      reset,
+   } = useForm({ mode: 'onChange' })
+
+   const input = {
+      username: {
+         ...register('username', {
+            required: 'Please enter your username',
+         }),
+      },
+      password: {
+         ...register('password', {
+            required: 'Please enter your password',
+         }),
+      },
+   }
+
+   const submitHandler = async (formData) => {
+      try {
+         await loginAdmin(formData)
+      } catch (error) {
+         console.log(error?.message || 'something went wrong')
+      }
+   }
+
+   const callAfterLogin = (userData) => {
+      reset()
+      dispatch(authActions.saveData(userData))
+
+      const role = userData.admin.roles.find((role) =>
+         Object.values(ROLES).includes(role)
+      )
+
+      switch (role) {
+         case ROLES.ADMIN:
+            navigate('/admin/dashboard', { replace: true })
+            break
+         case ROLES.SUPER_ADMIN:
+            navigate('/admin/dashboard')
+
+            break
+         default:
+            break
+      }
+   }
+
+   useEffect(() => {
+      if (isSuccess) callAfterLogin(data)
+      if (isError) console.log(`something went wrong`)
+   }, [isError, isSuccess])
+
    return (
       <div
          style={{
@@ -37,7 +103,7 @@ const Login = () => {
                   <CCardGroup>
                      <CCard className="p-4">
                         <CCardBody>
-                           <CForm>
+                           <CForm onSubmit={handleSubmit(submitHandler)}>
                               <h1>Login</h1>
                               <p className="text-medium-emphasis">
                                  Sign In to your account
@@ -47,8 +113,11 @@ const Login = () => {
                                     <CIcon icon={cilUser} />
                                  </CInputGroupText>
                                  <CFormInput
+                                    {...input.username}
                                     placeholder="Username"
                                     autoComplete="username"
+                                    error={errors?.username}
+                                    disabled={isLoading}
                                  />
                               </CInputGroup>
                               <CInputGroup className="mb-4">
@@ -56,14 +125,21 @@ const Login = () => {
                                     <CIcon icon={cilLockLocked} />
                                  </CInputGroupText>
                                  <CFormInput
+                                    {...input.password}
                                     type="password"
                                     placeholder="Password"
                                     autoComplete="current-password"
+                                    error={errors?.password}
+                                    disabled={isLoading}
                                  />
                               </CInputGroup>
                               <CRow>
                                  <CCol xs={6}>
-                                    <CButton color="primary" className="px-4">
+                                    <CButton
+                                       type="submit"
+                                       color="primary"
+                                       className="px-4"
+                                    >
                                        Login
                                     </CButton>
                                  </CCol>
