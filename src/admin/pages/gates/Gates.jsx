0e4568cb89d13562bafb247263b5708ca2fbcf9b
syimyk.jetibaev99@gmail.com <user@Syimyks-macbook.local>
2023-03-15 10:08:56 +0600

    feat: new add route gates

--- src/admin/pages/gates/Gates.jsx
+++ src/admin/pages/gates/Gates.jsx
@@ -1,7 +1,136 @@
-import React from 'react'
+/* eslint-disable no-unused-vars */
+import React, { useState } from 'react'
+
+import {
+   CCard,
+   CCardBody,
+   CCardHeader,
+   CCol,
+   CForm,
+   CButton,
+   CFormInput,
+   CFormLabel,
+   CFormControlWrapper,
+   CTableDataCell,
+   CTabPane,
+   CNavLink,
+   CNavItem,
+   CTabContent,
+   CNav,
+} from '@coreui/react'
+
+const GatesList = ({ gates }) => {
+   const fields = ['id', 'name', 'description']
+
+   return (
+      <CCard>
+         <CCardHeader>Gates</CCardHeader>
+         <CCardBody>
+            <CTableDataCell
+               items={gates}
+               fields={fields}
+               // hover
+               // striped
+               // bordered
+               size="sm"
+               itemsPerPage={5}
+               // pagination
+            />
+         </CCardBody>
+      </CCard>
+   )
+}
+
+const GatesForm = ({ onSubmit }) => {
+   const [newGate, setNewGate] = useState({ name: '', description: '' })
+   const handleNameChange = (event) =>
+      setNewGate({ ...newGate, name: event.target.value })
+   const handleDescriptionChange = (event) =>
+      setNewGate({ ...newGate, description: event.target.value })
+
+   const handleSubmit = (event) => {
+      event.preventDefault()
+      onSubmit(newGate)
+      setNewGate({ name: '', description: '' })
+   }
+
+   return (
+      <CCard>
+         <CCardHeader>Add Gate</CCardHeader>
+         <CCardBody>
+            <CForm onSubmit={handleSubmit}>
+               <CFormControlWrapper>
+                  <CFormLabel htmlFor="name">Name</CFormLabel>
+                  <CFormInput
+                     id="name"
+                     value={newGate.name}
+                     onChange={handleNameChange}
+                     required
+                  />
+               </CFormControlWrapper>
+               <CFormControlWrapper>
+                  <CFormLabel htmlFor="description">Description</CFormLabel>
+                  <CFormInput
+                     id="description"
+                     value={newGate.description}
+                     onChange={handleDescriptionChange}
+                     required
+                  />
+               </CFormControlWrapper>
+               <CButton type="submit" color="primary">
+                  Add Gate
+               </CButton>
+            </CForm>
+         </CCardBody>
+      </CCard>
+   )
+}
 
 const Gates = () => {
-   return <div>Gates</div>
+   const [activeTab, setActiveTab] = useState('list')
+   const [gates, setGates] = useState([
+      { id: 1, name: 'Gate A', description: 'This is Gate A.' },
+      { id: 2, name: 'Gate B', description: 'This is Gate B.' },
+   ])
+
+   const handleAddGate = (newGate) => {
+      setGates([...gates, { id: gates.length + 1, ...newGate }])
+   }
+
+   return (
+      <CCol>
+         <CTabContent activeTab={activeTab}>
+            <CNav
+               component="nav"
+               variant="pills"
+               className="flex-column flex-sm-row"
+            >
+               <CNavItem>
+                  <CNavLink onClick={() => setActiveTab('list')}>
+                     Gates List
+                  </CNavLink>
+               </CNavItem>
+               <CNavItem>
+                  <CNavLink onClick={() => setActiveTab('create')}>
+                     Create Gate
+                  </CNavLink>
+               </CNavItem>
+            </CNav>
+            <CTabContent>
+               <CTabPane data-tab="list">
+                  {/* {activeTab === 'list' && */}
+                  {/* <GatesList gates={gates} /> */}
+                  {/* } */}
+               </CTabPane>
+               <CTabPane data-tab="create">
+                  {/* {activeTab === 'create' && ( */}
+                  <GatesForm onSubmit={handleAddGate} />
+                  {/* )} */}
+               </CTabPane>
+            </CTabContent>
+         </CTabContent>
+      </CCol>
+   )
 }
 
 export default Gates
--- src/routes/admin-routes/routes.jsx
+++ src/routes/admin-routes/routes.jsx
@@ -4,12 +4,14 @@ import React from 'react'
 const Dashboard = React.lazy(() =>
    import('../../admin/pages/dashboard/Dashboard')
 )
+const Gates = React.lazy(() => import('../../admin/pages/gates/Gates'))
 
 // Base
 
 const routes = [
    { path: '/', exact: true, name: 'Home' },
    { path: '/dashboard', name: 'Dashboard', element: Dashboard },
+   { path: '/gates', name: 'Gates', element: Gates },
 ]
 
 export default routes
